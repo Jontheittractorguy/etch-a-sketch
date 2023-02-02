7709e59ed8856ad8459adf007250a417003e70a3
Jontheittractorguy <112268544+Jontheittractorguy@users.noreply.github.com>
2023-02-02 15:24:09 -0700

    made etch-a-sketch type page
    added css and javascript

--- etch.js
+++ etch.js
@@ -1,16 +1,29 @@
 let main = document.getElementById('main');
 
-for (let i=1;i<17;i++){
-    let row = document.createElement('dib');
-    row.classList.add('row' + i)
-    for (let e=1;e<17;e++){
-        let divs = document.createElement('div');
-        divs.classList.add("row");
-        row.appendChild(divs);
-    }
-    main.appendChild(row)
+function red(e){
+    e.target.classList.add('red')
 }
 
-let hover = document.querySelectorAll('row');
-hover.forEach(e=> e.addEventListener('mouseover',(event)=>
-    event.style.backgroundcolor = "red"))
+function grid(x,y){
+    for (let i=1;i<(x+1);i++){
+        let row = document.createElement('div');
+        row.classList.add('rows')
+        for (let e=1;e<(y+1);e++){
+            let divs = document.createElement('div');
+            divs.addEventListener('mouseover',red);
+            divs.classList.add("row");
+            row.appendChild(divs);
+        }
+    main.appendChild(row)
+    };
+}
+
+let input = parseInt(prompt("What size grid would you like",16));
+
+if (input>100){
+    prompt("Try again with lower number",20)
+} else if (isNaN(input)){
+    prompt("Thats not a Number...THIS is a number",40)
+} else {
+    grid(input,input)
+}
